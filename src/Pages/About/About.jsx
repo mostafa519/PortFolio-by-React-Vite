ddf0ccfbc0f2886af2ff217b3fc229266a85e706
Mustafa Ali <engmostafa51999@gmail.com>
2025-08-31 03:27:26 +0300

    cv

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -27,7 +27,8 @@ const About = () => {
                 Get IN Touch
               </a>
               <a
-                href="#"
+                href="/portfolio/public/Mustafa_Ali.Frontend_Developer.pdf"
+                download
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
